2cf285f13ebb8e6d066dcdb43ebf62e6afc8b369
Zaka <149817565+ZakaHaceCosas@users.noreply.github.com>
2024-01-18 18:27:16 +0100

    Update OcuFeed.js

--- src/utils/OcuFeed.js
+++ src/utils/OcuFeed.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-function OcuFeed() {
+const OcuFeed = () => {
+  const [datos, setDatos] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('./content.json');
+        const data = await response.json();
+        setDatos(data);
+      } catch (error) {
+        console.error('Error al acceder a content.json:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="feed">
+      {datos.map((entrada, index) => (
+        <div className="vidprev" key={index}>
+          <img src={entrada.miniatura} alt={`Miniatura ${index}`} />
+          <h3>{entrada.titulo}</h3>
+          <p>{entrada.descripcion}</p>
+          <div className="vidflex">
+            <p className="tag">{entrada.asig}</p>
+            <p className={`tag ${entrada.dificult}`}>{entrada.dificult_str}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default OcuFeed;
