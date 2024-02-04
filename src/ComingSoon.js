16368ea6a14216e32bf204b8e8bb80bfa49c93ad
Zaka <149817565+ZakaHaceCosas@users.noreply.github.com>
2024-02-04 17:10:21 +0100

    Update ComingSoon.js

--- src/ComingSoon.js
+++ src/ComingSoon.js
@@ -5,7 +5,7 @@ const ComingSoonPage = () => {
   return (
     <div className="seviene-container">
       <h1 className="seviene-label">Pronto disponible ;]</h1>
-      <img src="https://zakaprofeweb.vercel.app" alt="Logo de ZakaProfe" className="seviene-img" />
+      <img src="https://zakaprofeweb.vercel.app/logo512.png" alt="Logo de ZakaProfe" className="seviene-img" />
     </div>
   );
 }
