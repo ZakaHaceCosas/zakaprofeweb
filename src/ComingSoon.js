f6cb19e9bca203e2c167f6ecfcbb81bb212b1116
Zaka <149817565+ZakaHaceCosas@users.noreply.github.com>
2024-02-04 17:08:16 +0100

    Create ComingSoon.js

---
+++ src/ComingSoon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ComingSoon.css';
+
+const ComingSoonPage = () => {
+  return (
+    <div className="seviene-container">
+      <h1 className="seviene-label">Pronto disponible ;]</h1>
+      <img src="https://zakaprofeweb.vercel.app" alt="Logo de ZakaProfe" className="seviene-img" />
+    </div>
+  );
+}
+
+export default ComingSoonPage;
