df5197f4eb1335a31e8c5b6206e707fa301abb5d
Zaka <149817565+ZakaHaceCosas@users.noreply.github.com>
2024-03-05 17:00:35 +0100

    Delete src/utils/Logo.js

--- src/utils/Logo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Logo = () => (
-  <div className="zakaprofelogo">
-    <img src="https://zakaprofeweb.vercel.app/logo512.png" alt="Logo de ZakaProfe" />
-  </div>
-);
-
-export default Logo;
