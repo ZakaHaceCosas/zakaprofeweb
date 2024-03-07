1085097062aa959f25b3104cd937ac293289c9da
Zaka <149817565+ZakaHaceCosas@users.noreply.github.com>
2024-03-07 18:30:35 +0100

    Update randomvids.js

--- src/utils/randomvids.js
+++ src/utils/randomvids.js
@@ -1,7 +1,65 @@
-function RandomVideoFeed() {
+import React, { useState, useEffect } from 'react';
+
+const RandomVideoFeed = () => {
+  const videos = [
+    {
+      title: 'Sucesiones - 3º ESO',
+      thumbnail: 'https://zakaprofeweb.vercel.app/vid_mate_sucesiones_3eso.png',
+      url: 'https://youtu.be/DB5Njh4gbT0'
+    },
+    {
+      title: 'Reacciones químicas - 3º ESO',
+      thumbnail: 'https://zakaprofeweb.vercel.app/vid_fyq_reaccionesquimicas_3eso.png',
+      url: 'https://youtu.be/viivXTHioac'
+    },
+    {
+      title: 'Aparato Excretor - 3º ESO',
+      thumbnail: 'https://zakaprofeweb.vercel.app/vid_byg_apexcretor_3eso.png',
+      url: 'https://youtu.be/wZWR3jvr9mc'
+    },
+  ];
+
+  const [video1, setVideo1] = useState(null);
+  const [video2, setVideo2] = useState(null);
+
+  const RandomiseVids = () => {
+    const randomIndex1 = Math.floor(Math.random() * videos.length);
+    let randomIndex2 = Math.floor(Math.random() * videos.length);
+    while (randomIndex2 === randomIndex1) {
+      randomIndex2 = Math.floor(Math.random() * videos.length);
+    }
+    setVideo1(videos[randomIndex1]);
+    setVideo2(videos[randomIndex2]);
+  };
+
+  useEffect(() => {
+    RandomiseVids();
+  }, []);
+
   return (
-    <h1>a</h1>
+    <div className="randoms">
+      {video1 && (
+        <a href={video1.url} target="_blank" rel="noopener noreferrer">
+          <div className="video">
+            <div className="overlay">
+              <h1>Ver {video1.title} en YouTube</h1>
+            </div>
+            <img src={video1.thumbnail} alt={video1.title} />
+          </div>
+        </a>
+      )}
+      {video2 && (
+        <a href={video2.url} target="_blank" rel="noopener noreferrer">
+          <div className="video">
+            <div className="overlay">
+              <h1>Ver {video2.title} en YouTube</h1>
+            </div>
+            <img src={video2.thumbnail} alt={video2.title} />
+          </div>
+        </a>
+      )}
+    </div>
   );
-}
+};
 
 export default RandomVideoFeed;
