bbdd267687fb0b8087c21f78211e65021ae67c92
Zaka <149817565+ZakaHaceCosas@users.noreply.github.com>
2024-03-21 16:25:29 +0100

    Update zakavailable.js

--- src/utils/zakavailable.js
+++ src/utils/zakavailable.js
@@ -1,6 +1,56 @@
+import React, { useState, useEffect } from 'react';
+
 function Zakavailable() {
+  const [status, setStatus] = useState('');
+  const [statusp, setStatusp] = useState('');
+  const [statusClass, setStatusClass] = useState('');
+
+  useEffect(() => {
+    const getTimeStatus = () => {
+      const now = new Date();
+      const hour = now.getUTCHours() + 1;
+      let newStatus = '';
+      let newStatusp = '';
+      let newStatusClass = '';
+
+      if (hour >= 13 && hour < 21) {
+        newStatus = 'DISPONIBLE';
+        newStatusp = 'Si tienes cualquier duda, pregunta que te responderé.';
+        newStatusClass = 'disponible';
+      } else if (hour >= 21 && hour < 22) {
+        newStatus = 'MAS O MENOS';
+        newStatusp = 'Si tienes cualquier duda, pregunta que te responderé (igual tardo un poco)';
+        newStatusClass = 'maomeno';
+      } else if (hour >= 9 && hour < 13) {
+        newStatus = 'EN CLASE';
+        newStatusp = 'Estoy en clase.';
+        newStatusClass = 'ocupado';
+      } else {
+        newStatus = 'OCUPADO';
+        newStatusp = 'Esto... soy humano, tengo que dormir XD.';
+        newStatusClass = 'ocupado';
+      }
+
+      setStatus(newStatus);
+      setStatusp(newStatusp);
+      setStatusClass(newStatusClass);
+    };
+
+    const interval = setInterval(getTimeStatus, 60000); // actualiza cada 60 segundos
+    getTimeStatus();
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <h1>a</h1>
+      <div className="rowy">
+        <div className="oneside">
+          <div className={`bola ${statusClass}`}></div>
+          <h2 className={`nonboldh ${statusClass}`}>{status}</h2>
+        </div>
+        <p>
+          {statusp}
+        </p>
+      </div>
   );
 }
 
