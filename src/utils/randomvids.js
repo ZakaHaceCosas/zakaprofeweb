a19427d179b0eb603f04168a4afc6e40e39f108b
Zaka <149817565+ZakaHaceCosas@users.noreply.github.com>
2024-04-27 17:04:07 +0200

    Update randomvids.js

--- src/utils/randomvids.js
+++ src/utils/randomvids.js
@@ -27,6 +27,46 @@ const RandomVideoFeed = () => {
       thumbnail: 'https://zakaprofeweb.vercel.app/vid_bio_apcirculatorio_3eso.png',
       url: 'https://youtu.be/mNu2JVjy1v4'
     },
+    {
+      title: 'Sintaxis en Castellano - 3º ESO',
+      thumbnail: 'https://zakaprofeweb.vercel.app/vid_esp_sintaxis_3eso.png',
+      url: 'https://www.youtube.com/watch?v=CHgrGY5NdHU'
+    },
+    {
+      title: 'El átomo - 3º ESO',
+      thumbnail: 'https://zakaprofeweb.vercel.app/vid_fyq_teoriaatomica_3eso.png',
+      url: 'https://www.youtube.com/watch?v=Ouhi9hYIAi4'
+    },
+    {
+      title: 'La ciudad - 3º ESO',
+      thumbnail: 'https://zakaprofeweb.vercel.app/vid_cis_laciudad_3eso.png',
+      url: 'https://www.youtube.com/watch?v=HwObqxE3fgQ'
+    },
+    {
+      title: 'La materia y sus propiedades - 3º ESO',
+      thumbnail: 'https://zakaprofeweb.vercel.app/vid_fyq_lamateriaysusprops_3eso.png',
+      url: 'https://www.youtube.com/watch?v=OgVlCYGptZY'
+    },
+    {
+      title: 'La organización del ser humano - 3º ESO',
+      thumbnail: 'https://zakaprofeweb.vercel.app/vid_bio_laorgdelserhumano_3eso.png',
+      url: 'https://www.youtube.com/watch?v=gYXIwBPD05E'
+    },
+    {
+      title: 'Los números racionales - 3º ESO',
+      thumbnail: 'https://zakaprofeweb.vercel.app/vid_mate_numerosracionales_3eso.png',
+      url: 'https://www.youtube.com/watch?v=Aaj9n3m-Q-M'
+    },
+    {
+      title: 'El Estado - 3º ESO',
+      thumbnail: 'https://zakaprofeweb.vercel.app/vid_cis_elestado_3eso.png',
+      url: 'https://www.youtube.com/watch?v=5P6UevN82bQ'
+    },
+    {
+      title: 'Teorema de Pitágoras - 3º ESO',
+      thumbnail: 'https://zakaprofeweb.vercel.app/vid_mate_trmapitagoras_3eso.png',
+      url: 'https://www.youtube.com/watch?v=sJ_UhcUBId8'
+    },
   ];
 
   const [video1, setVideo1] = useState(null);
