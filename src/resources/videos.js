cff8ade6336408a90be6ce7724011d6be3b11b15
ZakaHaceCosas <optix@duck.com>
2024-05-22 18:10:19 +0200

    json fix

--- src/resources/videos.js
+++ src/resources/videos.js
@@ -3,70 +3,106 @@ const items = [
     {
         title: 'Sucesiones - 3º ESO',
         thumbnail: 'https://zakaprofeweb.vercel.app/vid_mate_sucesiones_3eso.png',
-        url: 'https://youtu.be/DB5Njh4gbT0'
+        url: 'https://youtu.be/DB5Njh4gbT0',
+        asig: 'Matemáticas',
+        pdfpro: null,
+        pdfnah: null
     },
     {
         title: 'Reacciones químicas - 3º ESO',
         thumbnail: 'https://zakaprofeweb.vercel.app/vid_fyq_reaccionesquimicas_3eso.png',
-        url: 'https://youtu.be/viivXTHioac'
+        url: 'https://youtu.be/viivXTHioac',
+        asig: 'Física y Química',
+        pdfpro: null,
+        pdfnah: null
     },
     {
         title: 'Aparato Excretor - 3º ESO',
         thumbnail: 'https://zakaprofeweb.vercel.app/vid_byg_apexcretor_3eso.png',
-        url: 'https://youtu.be/wZWR3jvr9mc'
+        url: 'https://youtu.be/wZWR3jvr9mc',
+        asig: 'Biología y Geología',
+        pdfpro: null,
+        pdfnah: null
     },
     {
         title: 'Verbos modales en inglés - 3º ESO',
         thumbnail: 'https://zakaprofeweb.vercel.app/vid_eng_modalverbs_3eso.png',
-        url: 'https://youtu.be/TSPC_rJzEX4'
+        url: 'https://youtu.be/TSPC_rJzEX4',
+        asig: 'Inglés',
+        pdfpro: null,
+        pdfnah: null
     },
     {
         title: 'Aparato Circulatorio - 3º ESO',
         thumbnail: 'https://zakaprofeweb.vercel.app/vid_bio_apcirculatorio_3eso.png',
-        url: 'https://youtu.be/mNu2JVjy1v4'
+        url: 'https://youtu.be/mNu2JVjy1v4',
+        asig: 'Biología y Geología',
+        pdfpro: null,
+        pdfnah: null
     },
     {
         title: 'Sintaxis en Castellano - 3º ESO',
         thumbnail: 'https://zakaprofeweb.vercel.app/vid_esp_sintaxis_3eso.png',
-        url: 'https://www.youtube.com/watch?v=CHgrGY5NdHU'
+        url: 'https://www.youtube.com/watch?v=CHgrGY5NdHU',
+        asig: 'Lengua Castellana',
+        pdfpro: null,
+        pdfnah: null
     },
     {
         title: 'El átomo - 3º ESO',
         thumbnail: 'https://zakaprofeweb.vercel.app/vid_fyq_teoriaatomica_3eso.png',
-        url: 'https://www.youtube.com/watch?v=Ouhi9hYIAi4'
+        url: 'https://www.youtube.com/watch?v=Ouhi9hYIAi4',
+        asig: 'Física y Química',
+        pdfpro: null,
+        pdfnah: null
     },
     {
         title: 'La ciudad - 3º ESO',
         thumbnail: 'https://zakaprofeweb.vercel.app/vid_cis_laciudad_3eso.png',
-        url: 'https://www.youtube.com/watch?v=HwObqxE3fgQ'
+        url: 'https://www.youtube.com/watch?v=HwObqxE3fgQ',
+        asig: 'Ciencias Sociales',
+        pdfpro: null,
+        pdfnah: null
     },
     {
         title: 'La materia y sus propiedades - 3º ESO',
         thumbnail: 'https://zakaprofeweb.vercel.app/vid_fyq_lamateriaysusprops_3eso.png',
-        url: 'https://www.youtube.com/watch?v=OgVlCYGptZY'
+        url: 'https://www.youtube.com/watch?v=OgVlCYGptZY',
+        asig: 'Física y Química',
+        pdfpro: null,
+        pdfnah: null
     },
     {
         title: 'La organización del ser humano - 3º ESO',
         thumbnail: 'https://zakaprofeweb.vercel.app/vid_bio_laorgdelserhumano_3eso.png',
-        url: 'https://www.youtube.com/watch?v=gYXIwBPD05E'
+        url: 'https://www.youtube.com/watch?v=gYXIwBPD05E',
+        asig: 'Biología y Geología',
+        pdfpro: null,
+        pdfnah: null
     },
     {
         title: 'Los números racionales - 3º ESO',
         thumbnail: 'https://zakaprofeweb.vercel.app/vid_mate_numerosracionales_3eso.png',
-        url: 'https://www.youtube.com/watch?v=Aaj9n3m-Q-M'
+        url: 'https://www.youtube.com/watch?v=Aaj9n3m-Q-M',
+        asig: 'Matemáticas',
+        pdfpro: null,
+        pdfnah: null
     },
     {
         title: 'El Estado - 3º ESO',
         thumbnail: 'https://zakaprofeweb.vercel.app/vid_cis_elestado_3eso.png',
-        url: 'https://www.youtube.com/watch?v=5P6UevN82bQ'
+        url: 'https://www.youtube.com/watch?v=5P6UevN82bQ',
+        asig: 'Ciencias Sociales',
+        pdfpro: null,
+        pdfnah: null
     },
     {
         title: 'Teorema de Pitágoras - 3º ESO',
         thumbnail: 'https://zakaprofeweb.vercel.app/vid_mate_trmapitagoras_3eso.png',
         url: 'https://www.youtube.com/watch?v=sJ_UhcUBId8',
         asig: 'Matemáticas',
-        pdfpro: 'https://zakaprofeweb.vercel.app/pdfpro_mate_trmapitagoras_3eso.png',
-        pdfnah: 'https://zakaprofeweb.vercel.app/pdfnah_mate_trmapitagoras_3eso.png'
+        pdfpro: null,
+        pdfnah: null
     },
 ];
 
