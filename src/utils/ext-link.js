414b48482ca0854125eeecb9030c94988d9c8d6e
Zaka <zakahacecosas@protonmail.com>
2025-02-10 17:50:14 +0100

    agregar enlace a correo-e

--- src/utils/ext-link.js
+++ src/utils/ext-link.js
@@ -1,7 +1,7 @@
 export default function ExternalLink({ url, children }) {
     return (
         <a
-            style={{ display: "flex", alignItems: "center" }}
+            style={{ display: "flex", alignItems: "center", gap: 5 }}
             href={url}
             target="_blank"
             rel="noopener noreferrer"
